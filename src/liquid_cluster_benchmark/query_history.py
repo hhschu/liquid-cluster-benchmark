ccc0c685c52437c6dfa2150e79225cb18f643d97
David Chu <david.chu@wallbox.com>
2023-11-29 12:14:43 +0100

    fix: remove non telemetry and duplicate queries

--- src/liquid_cluster_benchmark/query_history.py
+++ src/liquid_cluster_benchmark/query_history.py
@@ -1,6 +1,7 @@
 import os
 from datetime import datetime
 from pathlib import Path
+
 import requests
 
 host = "https://dbc-252cce92-d2bd.cloud.databricks.com"
@@ -18,16 +19,24 @@ params = {
     "max_results": 1000,
 }
 
+
+def is_valid(query):
+    query = query.lower()
+    return query.startswith("select") and "telemetry_v1" in query
+
+
 queries = []
 out_file = Path("static/queries.txt")
 has_next_page = True
 while has_next_page:
     resp = requests.get(host + path, headers=headers, json=params).json()
-    for q in resp["res"]:
-        query = q["query_text"].lower()
-        if query.startswith("select"):
-            queries.append(query)
+    for query in resp["res"]:
+        query_text = query["query_text"]
+        if is_valid(query_text):
+            queries.append(query_text)
     has_next_page = resp.get("has_next_page", False)
     params["page_token"] = resp.get("next_page_token")
 
+queries = set(queries)
+print("Number of queries:", len(queries))
 out_file.write_text("\n".join(queries))
